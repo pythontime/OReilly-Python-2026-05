e49be3a5ec1622a14a2aa3937c2bbbb441aa1788
Reuven Lerner <reuven@lerner.co.il>
2026-06-02 21:24:56 +0300

    gitautosync update 74

--- menu.py
+++ menu.py
@@ -7,4 +7,7 @@ def menu(options):
     
         print(f'Choose a valid option from {options}')    
 
-if __name__ ==         
+if __name__ == '__main__':   # __name__ is a variable, but '__main__' is a string!
+    choice = menu(['x', 'y', 'z'])
+    print(f'You chose {choice}')
+    
