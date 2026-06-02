937596b2736fda7fe45bbad2c75d3f43cf8977c0
Reuven Lerner <reuven@lerner.co.il>
2026-06-02 21:13:21 +0300

    gitautosync update 69

--- mymod.py
+++ mymod.py
@@ -1,4 +1,5 @@
-print(f'Hello from {__name__}!')
+if __name__ == '__main__':    # only run the block if we're running standalone
+    print(f'Hello from {__name__}!')
 
 x = 100
 
@@ -7,4 +8,5 @@ y = [10, 20, 30]
 def hello(name):
     return f'Hello, {name}, from mymod!'
 
-print(f'Thanks for visiting {__name__}!')
+if __name__ == '__main__':  # only run the block if we're running standalone
+    print(f'Thanks for visiting {__name__}!')
